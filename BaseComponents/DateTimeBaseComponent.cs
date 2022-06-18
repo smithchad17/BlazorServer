962867aece4dfabe616e76e0fa1cb1d674739337
Chad Smith <smithchad17@gmail.com>
2022-06-17 21:28:11 -0400

    Components and parameters

---
+++ BaseComponents/DateTimeBaseComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorServer.BaseComponents
+{
+    public class DateTimeBaseComponent : ComponentBase
+    {
+        public DateTime dateTime { get; set; }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            dateTime = DateTime.Now;
+        }
+    }
+}
